b80e0c10c66664dc6cf5f06c350fb43998d91631
Dubi Greenfeld <dubigreenfeld@gmail.com>
2018-03-16 00:11:09 +0200

    add numbers to markers on map

--- app/src/main/java/com/hackathon2018/androidacademytlv/mapapp/MapActivity.java
+++ app/src/main/java/com/hackathon2018/androidacademytlv/mapapp/MapActivity.java
@@ -1,5 +1,6 @@
 package com.hackathon2018.androidacademytlv.mapapp;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
@@ -10,10 +11,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Cap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.ui.IconGenerator;
 import com.hackathon2018.androidacademytlv.mapapp.Models.TripEvent;
 
 import java.util.ArrayList;
@@ -37,19 +43,49 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         long timeStamp = System.currentTimeMillis();
         mTripEvents.add(new TripEvent("Ozen Bar", timeStamp,timeStamp+1, 32.074208,34.7758732));
         mTripEvents.add(new TripEvent("Isrotel Royal Garden", timeStamp+2,timeStamp+3, 29.550476,34.9631261));
+        mTripEvents.add(new TripEvent("Lifta", timeStamp+4,timeStamp+5, 31.7953769,35.1962508));
+        mTripEvents.add(new TripEvent("Jericho", timeStamp+6,timeStamp+7, 31.8612161,35.461637));
+        mTripEvents.add(new TripEvent("HaBanim Garden", timeStamp+8,timeStamp+9, 32.6104967,35.2901403));
+        mTripEvents.add(new TripEvent("Spa Hotel Mizpe Hayamim", timeStamp+10,timeStamp+11, 32.9583682,35.5327069));
+        mTripEvents.add(new TripEvent("University Nahariya", timeStamp+12,timeStamp+13, 33.0222844,35.1078906));
+        mTripEvents.add(new TripEvent("Zar'it", timeStamp+14,timeStamp+15, 33.0920219,35.2663289));
+        mTripEvents.add(new TripEvent("1","Nahal Betzet", "", 33.0856492,35.2511153, 33.0823414,35.2275365, timeStamp+16,timeStamp+17, "", 0, "", "National Park", ""));
+        mTripEvents.add(new TripEvent("Bet She'arim National Park", timeStamp+18,timeStamp+19, 32.7037597,35.129801));
+        mTripEvents.add(new TripEvent("Residence Beach", timeStamp+20,timeStamp+21, 32.3279683,34.8492472));
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         PolylineOptions polylineOptions = new PolylineOptions().width(5).color(Color.RED);
+
+        //TODO: add bounderies to map
+        //TODO: sort events by time stamp
 //        new LatLngBounds();
-        // Add a marker in Sydney, Australia, and move the camera.
-        for (TripEvent event:
-             mTripEvents) {
-            LatLng markerPoint = new LatLng(event.startLatitude, event.startLongitude);
-            polylineOptions.add(markerPoint);
-            mMap.addMarker(new MarkerOptions().position(markerPoint).title(event.title));
+
+        for (int i=0; i < mTripEvents.size()-1; i++){
+            TripEvent event = mTripEvents.get(i);
+            LatLng markerStartPoint = new LatLng(event.startLatitude, event.startLongitude);
+            LatLng markerEndPoint = new LatLng(event.endLatitude, event.endLongitude);
+            polylineOptions.add(markerStartPoint);
+
+            IconGenerator icg = new IconGenerator(this);
+            Bitmap icon = icg.makeIcon(String.valueOf(i+1));
+            mMap.addMarker(
+                    new MarkerOptions()
+                            .position(markerStartPoint)
+                            .title(event.title)
+                            .icon(BitmapDescriptorFactory.fromBitmap(icon)));
+
+            if(markerEndPoint.latitude != 0 || markerEndPoint.longitude != 0){
+                icon = icg.makeIcon(String.valueOf(i+1));
+                polylineOptions.add(markerEndPoint);
+                mMap.addMarker(
+                        new MarkerOptions()
+                                .position(markerEndPoint)
+                                .title(event.title)
+                                .icon(BitmapDescriptorFactory.fromBitmap(icon)));
+            }
         }
 
         double startLatitudeZoom = mTripEvents.get(0).startLatitude;
