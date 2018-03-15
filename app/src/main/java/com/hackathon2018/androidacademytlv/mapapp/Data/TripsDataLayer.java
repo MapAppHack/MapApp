8cbf90d3186a1e550be5bf6343a778e48ce4d64a
Peter Oboukhovski <petero@mangodsp.com>
2018-03-15 21:30:38 +0200

    Add data manipulation functions fro trips, events and persons

--- app/src/main/java/com/hackathon2018/androidacademytlv/mapapp/Data/TripsDataLayer.java
+++ app/src/main/java/com/hackathon2018/androidacademytlv/mapapp/Data/TripsDataLayer.java
@@ -6,6 +6,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.hackathon2018.androidacademytlv.mapapp.Models.Person;
 import com.hackathon2018.androidacademytlv.mapapp.Models.Trip;
 import com.hackathon2018.androidacademytlv.mapapp.Models.TripEvent;
 
@@ -17,6 +18,11 @@ import java.util.List;
 
 public class TripsDataLayer {
 
+    public static final String EVENTS = "events";
+    public static final String TRIPS = "trips";
+    public static final String TRIP = "trip";
+    public static final String AUTHOR = "author";
+    private static final String PERSONS = "persons";
     private static TripsDataLayer mInstance;
     private DatabaseReference mDatabase;
 
@@ -36,7 +42,7 @@ public class TripsDataLayer {
 
     public void getTripsByUser(String user, final IItemAddedCallback<Trip> tripAddedCallback)
     {
-        ChildEventListener listener = mDatabase.child("trips").orderByChild("author").equalTo(user)
+        ChildEventListener listener = mDatabase.child(TRIPS).orderByChild(AUTHOR).equalTo(user)
                 .addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
@@ -65,9 +71,8 @@ public class TripsDataLayer {
         });
     }
 
-    public void getTripDetails(String trip, final IItemAddedCallback<Trip> tripAddedCallback)
-    {
-        mDatabase.child("trips").child(trip).addListenerForSingleValueEvent(new ValueEventListener() {
+    public void getTripDetails(String trip, final IItemAddedCallback<Trip> tripAddedCallback) {
+        mDatabase.child(TRIPS).child(trip).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Trip trip = dataSnapshot.getValue(Trip.class);
@@ -79,12 +84,68 @@ public class TripsDataLayer {
 
             }
         });
-
     }
-	
+
+    public void createTrip(Trip trip)
+    {
+        DatabaseReference ref = mDatabase.child(TRIPS).push();
+        trip.id=ref.getKey();
+        ref.setValue(trip);
+    }
+
+    public void saveTrip(Trip trip)
+    {
+        DatabaseReference ref = mDatabase.child(TRIPS).child(trip.id);
+        ref.setValue(trip);
+    }
+
+    public void deleteTrip(Trip trip)
+    {
+        DatabaseReference ref = mDatabase.child(TRIPS).child(trip.id);
+        ref.setValue(null);
+    }
+
+
+    public void createEvent(TripEvent tripEvent)
+    {
+        DatabaseReference ref = mDatabase.child(EVENTS).push();
+        tripEvent.id=ref.getKey();
+        ref.setValue(tripEvent);
+    }
+
+    public void saveEvent(TripEvent tripEvent)
+    {
+        DatabaseReference ref = mDatabase.child(EVENTS).child(tripEvent.id);
+        ref.setValue(tripEvent);
+    }
+
+    public void deleteEvent(TripEvent tripEvent)
+    {
+        DatabaseReference ref = mDatabase.child(EVENTS).child(tripEvent.id);
+        ref.setValue(null);
+    }
+    public void createPerson(Person person)
+    {
+        DatabaseReference ref = mDatabase.child(PERSONS).push();
+        person.id=ref.getKey();
+        ref.setValue(person);
+    }
+
+    public void savePerson(Person person)
+    {
+        DatabaseReference ref = mDatabase.child(PERSONS).child(person.id);
+        ref.setValue(person);
+    }
+
+    public void deletePerson(Person person)
+    {
+        DatabaseReference ref = mDatabase.child(PERSONS).child(person.id);
+        ref.setValue(null);
+    }
+
     public void getEventsByTrips(String trip, final IItemAddedCallback<TripEvent> tripAddedCallback)
     {
-        ChildEventListener listener = mDatabase.child("events").orderByChild("trip").equalTo(trip)
+        ChildEventListener listener = mDatabase.child(EVENTS).orderByChild(TRIP).equalTo(trip)
                 .addChildEventListener(new ChildEventListener() {
                     @Override
                     public void onChildAdded(DataSnapshot dataSnapshot, String s) {
