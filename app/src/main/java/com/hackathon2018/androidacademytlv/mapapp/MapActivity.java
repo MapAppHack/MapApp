48435583090fd5db10344635eb19369691508f86
Dubi Greenfeld <dubigreenfeld@gmail.com>
2018-03-15 17:16:59 +0200

    Add Launch activity for Trip List Activity Empty and add Map Activity

--- app/src/main/java/com/hackathon2018/androidacademytlv/mapapp/MapActivity.java
+++ app/src/main/java/com/hackathon2018/androidacademytlv/mapapp/MapActivity.java
@@ -1,13 +1,38 @@
 package com.hackathon2018.androidacademytlv.mapapp;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng petachTikva = new LatLng(32.0888389, 34.8679344);
+        mMap.addMarker(new MarkerOptions().position(petachTikva).title("Marker in Petach Tikva"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(petachTikva, 10));
     }
 }
