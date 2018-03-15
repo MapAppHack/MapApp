a160798215ded8a2c096fb7be5e77a655f579cbe
Dubi Greenfeld <dubigreenfeld@gmail.com>
2018-03-15 20:25:45 +0200

    add markers for trip events on map
    drew line between markers

--- app/src/main/java/com/hackathon2018/androidacademytlv/mapapp/TripListActivity.java
+++ app/src/main/java/com/hackathon2018/androidacademytlv/mapapp/TripListActivity.java
@@ -1,5 +1,6 @@
 package com.hackathon2018.androidacademytlv.mapapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class TripListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_list);
+
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 }
