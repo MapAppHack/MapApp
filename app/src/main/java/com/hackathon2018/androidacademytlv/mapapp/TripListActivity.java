155c4b6b9322e1bb4fe773825293435b33061e8b
Dubi Greenfeld <dubigreenfeld@gmail.com>
2018-03-16 10:04:32 +0200

    handle intent trip

--- app/src/main/java/com/hackathon2018/androidacademytlv/mapapp/TripListActivity.java
+++ app/src/main/java/com/hackathon2018/androidacademytlv/mapapp/TripListActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hackathon2018.androidacademytlv.mapapp.Data.IItemAddedCallback;
+import com.hackathon2018.androidacademytlv.mapapp.Data.TripsDataLayer;
+import com.hackathon2018.androidacademytlv.mapapp.Models.Trip;
+
 public class TripListActivity extends AppCompatActivity {
 
     @Override
@@ -11,7 +15,13 @@ public class TripListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_list);
 
-        Intent intent = new Intent(this, MapActivity.class);
-        startActivity(intent);
+        final Intent intent = new Intent(this, MapActivity.class);
+        TripsDataLayer.getInstance().getTripsByUser("user1", new IItemAddedCallback<Trip>() {
+            @Override
+            public void onItemAdded(Trip item) {
+                intent.putExtra(Trip.EXTRA_KEY, item);
+                startActivity(intent);
+            }
+        });
     }
 }
